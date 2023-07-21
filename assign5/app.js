4bcbf189373459ce26a1de1fba8fc884889c6558
Allassane <dickoallassane1997@gmail.com>
2023-07-21 18:51:45 +0100

    Setup routes

--- assign5/app.js
+++ assign5/app.js
@@ -1,2 +1,13 @@
 const express = require('express')
+const app = express()
+const adminRoutes = require('./src/routes/index')
 
+/* setup */
+app.set('view engine','ejs')
+
+/* middleware */
+/* routes */
+app.use(adminRoutes)
+
+
+app.listen(3005)
