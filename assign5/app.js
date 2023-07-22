f35ec47f1e1488c120c575abb1943e215f44af56
Allassane <dickoallassane1997@gmail.com>
2023-07-22 16:45:20 +0100

    Update project to use ejs

--- assign5/app.js
+++ assign5/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const adminRoutes = require('./src/routes/index')
+const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/index')
 
 /* setup */
 app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended: false}))
 
 /* middleware */
 /* routes */
 app.use(adminRoutes)
-
 
 app.listen(3005)
--- assign5/routes/index.js
+++ assign5/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/', (req, res, next)=>{
-  console.log("get Home")
+  res.render('index.ejs')
 })
 
 module.exports = router
