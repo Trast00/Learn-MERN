3f124e6c7e7c8eb046230d565d66725ab79a8ded
Allassane <dickoallassane1997@gmail.com>
2023-07-23 20:33:15 +0100

    Update project to make public folder work

--- assign5/app.js
+++ assign5/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/index')
+
 
 /* setup */
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 /* middleware */
 /* routes */
