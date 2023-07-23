a9130f9e9e17d81905cc40f17d45b0affe653ad4
Allassane <dickoallassane1997@gmail.com>
2023-07-23 20:17:43 +0100

    Add font family index.js

--- assign4/routes/users.js
+++ assign4/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {listUsers} = require('../routes/admin.js')
 
-router.get('/', (req, res, next) => {
+router.get('/users', (req, res, next) => {
   res.render("users", {pageTitle: "List Users:" , listUsers: listUsers})
 })
 
