782663e7f4e69ffccbaff2e4eda9b8ea504cdb5d
Allassane <dickoallassane1997@gmail.com>
2023-07-24 20:05:57 +0100

    Update user.js to fix route bug

--- assign5/routes/user.js
+++ assign5/routes/user.js
@@ -20,6 +20,7 @@ router.use('/users/:userId', (req, res, next) => {
   const user = listUsers[req.params.userId-1]
   req.currentUser = user
   console.log(`User id is ${user.id}, ${user.name}`)
+  res.render('shops.ejs')
 })
 
 module.exports = router
