c82076878199b8d14d838d9e31331d7df04755bd
Allassane <dickoallassane1997@gmail.com>
2023-08-22 20:29:30 +0100

    Update cart to ES6

--- assign5/models/cart.js
+++ assign5/models/cart.js
@@ -1,6 +1,5 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
+const Sequelize = require('sequelize')
+const sequelize = require('../utils/database')
 
 const Cart = sequelize.define('cart', {
   id: {
