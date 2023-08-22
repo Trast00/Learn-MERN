4f00e71af7abb1aa642606a839ce682779b2ea3a
Allassane <dickoallassane1997@gmail.com>
2023-08-22 20:26:13 +0100

    Update animal to pet, add es6 import

--- assign5/controllers/pet.js
+++ assign5/controllers/pet.js
@@ -1,5 +1,28 @@
-export const getAll = () => {
-  
+const listAnimals = [
+  {
+    id: 1,
+    name: "Mouse",
+    imgUrl: "aaa",
+    ownerId: 1,
+  },
+  {
+    id: 2,
+    name: "Cat",
+    imgUrl: "aaa",
+    ownerId: 1,
+  },
+  {
+    id: 3,
+    name: "Pigeon",
+    imgUrl: "aaa",
+    ownerId: 2,
+  }
+]
+
+export const getAll = (req, res, next) => {
+  res.render('shops.ejs', {
+    pageTitle: "Shop",
+    listAnimals: listAnimals})
 }
 
 export const getById = () => {
