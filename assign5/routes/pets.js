7d7f2b7be6298fe528e381263e9de93bf683e944
Allassane <dickoallassane1997@gmail.com>
2023-08-23 16:28:33 +0100

    Update pet routes

--- assign5/routes/pets.js
+++ assign5/routes/pets.js
@@ -2,6 +2,6 @@ import express from 'express'
 const router = express.Router()
 import * as petsController from '../controllers/pet.js'
 
-router.use('/pets', petsController.getAll)
+router.use('/pets', petsController.index)
 
 export default router
