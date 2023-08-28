b0c30adadece632283990793087aa445b102b5f7
Allassane <dickoallassane1997@gmail.com>
2023-08-28 20:25:44 +0100

    Add cart controller

--- assign5/controllers/cart.js
+++ assign5/controllers/cart.js
@@ -1,15 +1,3 @@
-export const getCartByUserId= (id) => {
-
-}
-
-export const addPet = (pet) => {
-
-}
-
-export const removePet = (pet) => {
-
-}
-
-export const removeAll = (pet) => {
-
+export const index = (req, res, next) => {
+  res.render('cart.ejs')
 }
