ab37de959e0aabd9b99795a89dadfccb0932644a
Allassane <dickoallassane1997@gmail.com>
2023-09-09 19:49:00 +0100

    Update models relations

--- assign5/app.js
+++ assign5/app.js
@@ -47,10 +47,10 @@ app.use(userRoutes)
 app.use(shopRoute)
 app.use(favoriteRoutes)
 
-User.hasMany(Favorite)
 Pet.hasMany(Favorite)
-Favorite.belongsTo(User, {constrainst: true, onDelete: 'CASCADE'})
 Favorite.belongsTo(Pet, {constrainst: true, onDelete: 'CASCADE'})
+User.hasMany(Favorite)
+Favorite.belongsTo(User, {constrainst: true, onDelete: 'CASCADE'})
 
 sequelize.sync().then(_ => {
   app.listen(3005)
