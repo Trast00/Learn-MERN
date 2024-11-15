69055b45020a619c1a79ca748111229af5db49df
unknown <dickoallassane1997@gmail.com>
2024-11-15 18:12:07 +0100

    Add authentification folder for auth session

---
+++ authentification/controllers/auth.js
@@ -0,0 +1,57 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+
+  const {name, password, confirmPassword} = req.body
+
+  User.findOne({email: email}).then(data => {
+    if(data) {
+      // user already exist
+      redirect('/signup')
+    }
+    const user = new User(
+      name,
+      email,
+      password
+    )
+  }).then(result => {
+    console.log(result)
+    res.redirect('/login')
+  })
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
